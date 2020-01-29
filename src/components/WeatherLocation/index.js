2bf1d6bbe10431c2d5ee71cf3a971216ac09c71a
adrian <>
2020-01-29 11:19:59 +0100

    39

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,7 +3,7 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
 import {
-    SUN,
+    SUN, WINDY,
 } from '../../constants/weathers';
 
 const data = {
@@ -13,13 +13,39 @@ const data = {
     wind: '10 m/s',
 }
 
+const data2 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '10 m/s',
+}
+
 class WeatherLocation extends Component {
-    
+
+    constructor() {
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data,
+        }
+    }
+
+    handleUpdateClick = () => {
+        console.log("actualizado");
+
+        this.setState({
+            city: 'Buenos Aires!',
+            data: data2,
+        });
+    }
+
     render() {
+        const { city, data } = this.state;
         return (
             <div className="weatherLocationCont">
-                <Location city={'Sevilla'}></Location>
+                <Location city={city}></Location>
                 <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
             </div>
         );
     }
