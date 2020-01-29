53c696ec4625770ea10b15106da168d78efa1298
adrian <>
2020-01-29 10:21:51 +0100

    36

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature'
 import WeatherExtraInfo from './WeatherExtraInfo';
-
-// Vamos a importar las constantes
-// En el from debemos subir un nivel con ../
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY,
-} from '../../../constants/weathers' ;
 import './styles.css'
 
 
 // Ahora en weatherState ponemos directamente el nombre de la constante
-const WeatherData = () => (
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind} }) => (
     <div className="weatherDataCont">
         <WeatherTemperature
-            temperature={20}
-            weatherState={RAIN} />
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"}></WeatherExtraInfo>
+            temperature={temperature}
+            weatherState={weatherState} />
+        <WeatherExtraInfo humidity={humidity} wind={wind}></WeatherExtraInfo>
     </div>
 );
 
