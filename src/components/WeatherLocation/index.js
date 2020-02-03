ce5a9d7514e68e0e847897fd9567237dfe2d7ae1
adrian <>
2020-02-03 11:27:56 +0100

    66

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -45,11 +45,11 @@ class WeatherLocation extends Component {
         });
     }
 
-    render() {
-        console.log("render");
+    render = () => {
+        const { onWeatherLocationClick } = this.props;
         const { city, data } = this.state;
         return (
-            <div className="weatherLocationCont">
+            <div className="weatherLocationCont" onClick={onWeatherLocationClick} >
                 <Location city={city}></Location>
 
                 {data ?
@@ -63,6 +63,7 @@ class WeatherLocation extends Component {
 
 WeatherLocation.propTypes = {
     city: PropTypes.string.isRequired,
+    onWeatherLocationClick: PropTypes.func,
 }
 // Exportamos la constante
 export default WeatherLocation;
