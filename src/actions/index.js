2ba1dc619f66ccf2d0493a9d96a42e2c376e3468
adrian <>
2020-02-05 14:32:49 +0100

    131

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,24 @@
+import transformForecast from './../services/transformForecast';
+
 export const SET_CITY = 'SET_CITY';
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
 
 export const setCity = payload => ({ type: SET_CITY, payload });
-
+const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload })
 
 
 const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
 const url = "http://api.openweathermap.org/data/2.5/forecast";
 
 
-export const fetchForecast = payload => {
+export const setSelectedCity = payload => {
     return dispatch => {
-        const url_forecast = `${url}?q=${city}&appid=${api_key}`;
+        const url_forecast = `${url}?q=${payload}&appid=${api_key}`;
 
         // activar en el estado un indicador de busqueda de datos
+        dispatch(setCity(payload));
 
-        fetch(url_forecast).then(
+        return fetch(url_forecast).then(
             data => (data.json())
         ).then(
             weather_data => {
@@ -22,9 +26,9 @@ export const fetchForecast = payload => {
                 console.log(forecastData);
 
                 // modificar el estado con el resultado de la promise (fetch)
+                dispatch(setForecastData({city: payload, forecastData }))
             }
         );
 
-        return;
     };
 };
