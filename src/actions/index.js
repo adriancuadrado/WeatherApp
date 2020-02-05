bbd77aa2787f39fd42e5951ac539b030902b4a12
adrian <>
2020-02-05 13:26:20 +0100

    128

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,30 @@
 export const SET_CITY = 'SET_CITY';
 
 export const setCity = payload => ({ type: SET_CITY, payload });
+
+
+
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+
+
+export const fetchForecast = payload => {
+    return dispatch => {
+        const url_forecast = `${url}?q=${city}&appid=${api_key}`;
+
+        // activar en el estado un indicador de busqueda de datos
+
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
+
+                // modificar el estado con el resultado de la promise (fetch)
+            }
+        );
+
+        return;
+    };
+};
