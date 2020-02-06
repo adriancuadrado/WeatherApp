89f855e6bbd2c1778a87bc24ce8d7067d8ef3def
adrian <>
2020-02-06 12:43:04 +0100

    140

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,21 @@
 import transformForecast from './../services/transformForecast';
+import transformWeather from './../services/transformWeather';
 
 export const SET_CITY = 'SET_CITY';
 export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
-export const SET_WEATHER = 'SET_WEATHER';
 
-export const setCity = payload => ({ type: SET_CITY, payload });
+export const GET_WEATHER_CITY = 'GET_WEATHER_CITY';
+export const SET_WEATHER_CITY = 'SET_WEATHER_CITY';
+
+const setCity = payload => ({ type: SET_CITY, payload });
 const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload })
 
+const getWeatherCity = payload => ({type: GET_WEATHER_CITY, payload });
+const setWeatherCity = payload => ({type: SET_WEATHER_CITY, payload });
 
 const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
 const url = "http://api.openweathermap.org/data/2.5/forecast";
+const url_weather = "http://api.openweathermap.org/data/2.5/weather";
 
 
 export const setSelectedCity = payload => {
@@ -36,6 +42,21 @@ export const setSelectedCity = payload => {
 
 export const setWeather = payload => {
 
+    return dispatch => {
+        payload.forEach(city => {
+
+            dispatch(getWeatherCity(city));
+
+            const api_weather = `${url_weather}?q=${city}&appid=${api_key}`;
+            fetch(api_weather).then(resolve => {
+                return resolve.json();
+            }).then(weather_data => {
+                const data = transformWeather(weather_data);
+
+                dispatch(setWeatherCity({city, data}));
+            });
+        });
+    }
     /* 
     const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
     const url = "https://api.openweathermap.org/data/2.5/weather";
@@ -44,11 +65,9 @@ export const setWeather = payload => {
         const api_weather = `${url}?q=${city}&appid=${api_key}`;
         fetch(api_weather).then(resolve => {
             return resolve.json();
-        }).then(data => {
-            const newWeather = transformWeather(data);
-            this.setState({
-                data: newWeather,
-            });
+        }).then(weather_data => {
+            const data = transformWeather(weather_data);
+            this.setState({ data });
         });
     }
      */
